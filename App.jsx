6ee1bb5adcf80750ef1fa8e575897cd2b755326c
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-09-12 20:37:22 +0300

    Add functionality to theme toggler

--- App.jsx
+++ App.jsx
@@ -1,12 +1,18 @@
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import { useState } from "react";
 
 function App(){
+    const [darkMode, setDarkMode] = useState(true)
+
+
+    function toggleDarkMode(){
+        setDarkMode(prevMode => !prevMode)
+    }
     return(
         <div className="container">
-            <Navbar />
-            <div className="break"></div>
-            <Main />
+            <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+            <Main darkMode={darkMode}/>
         </div>
     )
 }
